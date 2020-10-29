b06b844a610d1b6b8aecdbc6f9d3d5887932c1e6
Hector Vasquez <hvasquezdev@gmail.com>
2020-10-29 19:57:11 -0400

    Create game board scaffold

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
 <template>
   <h1>Peek a card</h1>
+
+  <section class="game-board">
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+  </section>
 </template>
 
 <script>
@@ -16,5 +35,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.card {
+  border: 5px solid #ccc;
+}
+
+.game-board {
+  display: grid;
+  grid-template-columns: repeat(4, 100px);
+  grid-template-rows: repeat(4, 100px);
+  column-gap: 30px;
+  row-gap: 30px;
+  justify-content: center;
 }
 </style>
