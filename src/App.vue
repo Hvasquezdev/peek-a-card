9ecf13891df139d2d6ab92881013c86001f4badf
Hector Vasquez <hvasquezdev@gmail.com>
2020-11-14 15:28:40 -0400

    style(app.vue): lint main app code

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <template>
   <h1 class="game-title">Peek a card</h1>
 
-  <transition-group
-    tag="section"
-    class="game-board"
-    name="shuffle-cards"
-  >
+  <transition-group tag="section" class="game-board" name="shuffle-cards">
     <Card
-      v-for="(card) in cardList"
+      v-for="card in cardList"
       :key="`card-${card.id}-${card.value}`"
       :value="card.value"
       :visible="card.visible"
@@ -22,7 +18,11 @@
     {{ status }}
   </h2>
 
-  <button v-if="!isPlaying" class="game-button game-play-btn" @click="startGame">
+  <button
+    v-if="!isPlaying"
+    class="game-button game-play-btn"
+    @click="startGame"
+  >
     <img src="/images/play.svg" alt="Play icon" />
     Play game
   </button>
@@ -33,37 +33,39 @@
 </template>
 
 <script>
-import { ref, watch, computed, onBeforeMount } from 'vue'
-import { launchConfetti } from '@/utils/confetti'
-import Card from '@/components/Card'
+import { ref, watch, computed, onBeforeMount } from 'vue';
+import { launchConfetti } from '@/utils/confetti';
+import Card from '@/components/Card';
 
 export default {
   name: 'App',
   components: {
-    Card
+    Card,
   },
   setup() {
-    const cardList = ref([])
-    const userSelection = ref([])
-    const isPlaying = ref(false)
+    const cardList = ref([]);
+    const userSelection = ref([]);
+    const isPlaying = ref(false);
 
-    const uuid = () => `_${Math.random().toString(36).substr(2, 9)}`
+    const uuid = () =>
+      `_${Math.random()
+        .toString(36)
+        .substr(2, 9)}`;
 
     const status = computed(() => {
       if (remainingPairs.value === 0) {
-        return 'Player wins!'
+        return 'Player wins!';
       }
 
-      return `Remaining pairs: ${remainingPairs.value}`
-    })
+      return `Remaining pairs: ${remainingPairs.value}`;
+    });
 
     const remainingPairs = computed(() => {
-      const list = Array.from(cardList.value)
-      const remainingCards = list.filter((card) => (
-        card.matched === false
-      )).length
-      return remainingCards / 2
-    })
+      const list = Array.from(cardList.value);
+      const remainingCards = list.filter((card) => card.matched === false)
+        .length;
+      return remainingCards / 2;
+    });
 
     const generateCardList = () => {
       const cards = [
@@ -74,115 +76,119 @@ export default {
         'cupcake',
         'moon',
         'pumpkin',
-        'witch-hat'
-      ]
+        'witch-hat',
+      ];
 
       cards.forEach((card) => {
-        const listSize = cardList.value.length
+        const listSize = cardList.value.length;
         const value = {
           value: card,
           visible: false,
           position: null,
-          matched: false
-        }
+          matched: false,
+        };
 
-        const getRandomVisible = () => Math.floor(Math.random() * 10) % 2 === 0
+        const getRandomVisible = () => Math.floor(Math.random() * 10) % 2 === 0;
 
         cardList.value[listSize] = {
           ...value,
           id: uuid(),
           position: listSize,
-          visible: getRandomVisible()
-        }
+          visible: getRandomVisible(),
+        };
         cardList.value[listSize + 1] = {
           ...value,
           id: uuid(),
           position: listSize + 1,
-          visible: getRandomVisible()
-        }
-      })
-    }
+          visible: getRandomVisible(),
+        };
+      });
+    };
 
     const shuffleArray = (arr, cb) => {
-      const arrToShuffle = Array.from(arr)
+      const arrToShuffle = Array.from(arr);
 
-      for (let i = arrToShuffle.length -1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1))
+      for (let i = arrToShuffle.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
 
         if (j !== i) {
-          [arrToShuffle[i], arrToShuffle[j]] = [arrToShuffle[j], arrToShuffle[i]]
+          [arrToShuffle[i], arrToShuffle[j]] = [
+            arrToShuffle[j],
+            arrToShuffle[i],
+          ];
 
           if (cb) {
-            cb(arrToShuffle[i], arrToShuffle[j], i, j)
+            cb(arrToShuffle[i], arrToShuffle[j], i, j);
           }
         }
       }
 
-      return arrToShuffle
-    }
+      return arrToShuffle;
+    };
 
     const suffleCards = () => {
       cardList.value = shuffleArray(cardList.value, (first, second, i, j) => {
-        first.position = i
-        second.position = j
-      })
-    }
+        first.position = i;
+        second.position = j;
+      });
+    };
 
     const startGame = () => {
-      restartGame()
-      isPlaying.value = true
-    }
+      restartGame();
+      isPlaying.value = true;
+    };
 
     const restartGame = () => {
-      suffleCards()
+      suffleCards();
 
-      cardList.value = cardList.value.map(card => ({
+      cardList.value = cardList.value.map((card) => ({
         ...card,
         matched: false,
-        visible: false
-      }))
-    }
+        visible: false,
+      }));
+    };
 
     const flipCard = (payload) => {
-      cardList.value[payload.position].visible = true
+      cardList.value[payload.position].visible = true;
 
-      userSelection.value = [
-        ...userSelection.value,
-        payload
-      ]
-    }
+      userSelection.value = [...userSelection.value, payload];
+    };
 
     onBeforeMount(() => {
-      generateCardList()
-      suffleCards()
-    })
+      generateCardList();
+      suffleCards();
+    });
 
     watch(remainingPairs, (current) => {
       if (current === 0) {
-        launchConfetti()
+        launchConfetti();
       }
-    })
+    });
 
-    watch(userSelection, (current) => {
-      if (current.length === 2) {
-        const cardOne = current[0]
-        const cardTwo = current[1]
+    watch(
+      userSelection,
+      (current) => {
+        if (current.length === 2) {
+          const cardOne = current[0];
+          const cardTwo = current[1];
 
-        if (cardOne.faceValue === cardTwo.faceValue) {
-          cardList.value[cardOne.position].matched = true
-          cardList.value[cardTwo.position].matched = true
-        } else {
-          const timeOut = setTimeout(() => {
-            cardList.value[cardOne.position].visible = false
-            cardList.value[cardTwo.position].visible = false
-            
-            clearTimeout(timeOut)
-          }, 1000);
+          if (cardOne.faceValue === cardTwo.faceValue) {
+            cardList.value[cardOne.position].matched = true;
+            cardList.value[cardTwo.position].matched = true;
+          } else {
+            const timeOut = setTimeout(() => {
+              cardList.value[cardOne.position].visible = false;
+              cardList.value[cardTwo.position].visible = false;
+
+              clearTimeout(timeOut);
+            }, 1000);
+          }
+
+          userSelection.value.length = 0;
         }
-
-        userSelection.value.length = 0
-      }
-    }, { deep: true })
+      },
+      { deep: true }
+    );
 
     return {
       cardList,
@@ -191,10 +197,10 @@ export default {
       remainingPairs,
       restartGame,
       startGame,
-      isPlaying
-    }
-  }
-}
+      isPlaying,
+    };
+  },
+};
 </script>
 
 <style>
@@ -262,7 +268,6 @@ export default {
 .game-play-btn {
   margin-top: 20px;
 }
-
 
 .game-restart-btn {
   background: #ffd600;
